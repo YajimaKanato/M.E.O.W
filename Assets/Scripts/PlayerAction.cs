184588eb4d0f5e81f099fcbc92fc457f3eb4def1
KNTy1018 <soramametaro1018@gmail.com>
2025-10-01 22:46:04 +0900

    [add]ダッシュ実装

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -4,13 +4,13 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerAction : MonoBehaviour
 {
-    [SerializeField] float _speed = 5;
-    [SerializeField] float _dashSpeed = 10;
+    [SerializeField] float _walkSpeed = 5;
+    [SerializeField] float _runSpeed = 10;
     [SerializeField] float _jump = 5;
     [SerializeField] float _gravity = 9.8f;
     [SerializeField] float _gravityScale = 5;
 
-    InputAction _moveAct, _jumpAct;
+    InputAction _moveAct, _jumpAct, _runAct;
     Rigidbody2D _rb2d;
 
     Vector3 _move;
@@ -21,13 +21,15 @@ public class PlayerAction : MonoBehaviour
         _jumpAct = InputSystem.actions.FindAction("Jump");
         //Jump‚ÉŠ„‚è“–‚Ä‚ç‚ê‚½‚à‚Ì‚ð‰Ÿ‚µ‚½uŠÔ‚ÉŒÄ‚Î‚ê‚é
         _jumpAct.started += a => _rb2d.AddForce(Vector3.up * _jump, ForceMode2D.Impulse);
+        _runAct = InputSystem.actions.FindAction("Run");
         _rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _move = _moveAct.ReadValue<Vector2>() * _speed;
+        _move = _moveAct.ReadValue<Vector2>();
+        _move *= _runAct.IsPressed() ? _runSpeed : _walkSpeed;
 
         _move.y = _rb2d.linearVelocityY;
         if (_move.y != 0)
