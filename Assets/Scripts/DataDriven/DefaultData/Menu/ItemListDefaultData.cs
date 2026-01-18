9c94fc05c4ab01fa81dd5a633877ba0ec5eb2baf
KNTy1018 <soramametaro1018@gmail.com>
2026-01-19 00:10:39 +0900

    [update]データ面ほぼ完成Part2

--- Assets/Scripts/DataDriven/DefaultData/Menu/ItemListDefaultData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemListDefaultData", menuName = "Scriptable Objects/ItemListDefaultData")]
-public class ItemListDefaultData : ScriptableObject
-{
-    
-}
